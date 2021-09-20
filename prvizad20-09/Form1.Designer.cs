ee74d06dab7baa310605cea794f05a82d13f2e4d
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-09-20 09:04:12 +0200

    Add files via upload

---
+++ prvizad20-09/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace prvizad20_09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOtvori = new System.Windows.Forms.Button();
+            this.textBoxRezultati = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOtvori
+            // 
+            this.buttonOtvori.Location = new System.Drawing.Point(139, 48);
+            this.buttonOtvori.Name = "buttonOtvori";
+            this.buttonOtvori.Size = new System.Drawing.Size(160, 23);
+            this.buttonOtvori.TabIndex = 0;
+            this.buttonOtvori.Text = "Otvorite MessageBox";
+            this.buttonOtvori.UseVisualStyleBackColor = true;
+            this.buttonOtvori.Click += new System.EventHandler(this.ButtonOtvori_Click);
+            // 
+            // textBoxRezultati
+            // 
+            this.textBoxRezultati.Location = new System.Drawing.Point(111, 112);
+            this.textBoxRezultati.Multiline = true;
+            this.textBoxRezultati.Name = "textBoxRezultati";
+            this.textBoxRezultati.Size = new System.Drawing.Size(224, 115);
+            this.textBoxRezultati.TabIndex = 1;
+            this.textBoxRezultati.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 288);
+            this.Controls.Add(this.textBoxRezultati);
+            this.Controls.Add(this.buttonOtvori);
+            this.Name = "Form1";
+            this.Text = "Upotreba DialogResult enumeracije ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOtvori;
+        private System.Windows.Forms.TextBox textBoxRezultati;
+    }
+}
+
---
+++ prvizad20-09/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prvizad20_09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void ButtonOtvori_Click(object sender, EventArgs e)
+        {
+            string poruka = "Odaberite Yes / No / Cancel";
+            string naslov = "Naslovna linija Dijaloga za poruke";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+            MessageBox.Show(poruka, naslov, buttons, ikona);
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+            switch (rez)
+            {
+                case DialogResult.Yes:
+                    textBoxRezultati.Text = "Izabrali ste dugme YES";
+                    break;
+                case DialogResult.No:
+                    textBoxRezultati.Text = "Izabrali ste dugme NO";
+                    break;
+                case DialogResult.Cancel:
+                    textBoxRezultati.Text = "Izabrali ste dugme CANCEL";
+                    break;
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
